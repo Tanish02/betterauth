e3b620dfda6249e244fa57af0b82d6ac8ca58617
Tanish Sharma <tanishsharma080@gmail.com>
2025-09-30 03:09:54 +0530

    add login/sigIn page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,7 +1,64 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { redirect, useRouter } from "next/navigation";
 import React from "react";
+// import SignInPage from "./page";
 
 const SignInPage = () => {
-  return <div> SignInPage </div>;
+  const router = useRouter();
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+    // SUBMIT LOGIC
+
+    const { data, error } = await authClient.signIn.email({
+      email: email,
+      password: password,
+      rememberMe: true,
+      callbackURL: "/dashboard",
+    });
+    console.log("data", data);
+  }
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <form
+        className="flex flex-col gap-5 p-7 bg-zinc-600 text-white w-96 rounded shadow-md mx-auto"
+        onSubmit={handleSubmit}
+      >
+        <h1> SignIn </h1>
+        {/* <input
+          className="bg-zinc-700 border text-white-600 p-2 rounded"
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        /> */}
+        <input
+          className="bg-zinc-700 border text-white-600 p-2 rounded"
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="bg-zinc-700 border text-white-600 p-2 rounded"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+        >
+          SignIn
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SignInPage;
