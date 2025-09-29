760fed59768de5fd8c144c571ca3c4f3e85ad9ad
Tanish Sharma <tanishsharma080@gmail.com>
2025-09-30 02:38:19 +0530

    add logout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import { Sign } from "crypto";
+import Logout from "./components/logout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
           <Link href={"/dashboard"}>Dashboard</Link>
           <Link href={"/signin"}>SignIn</Link>
           <Link href={"/signup"}>SignUp</Link>
+          <Logout />
         </nav>
         {children}
       </body>
